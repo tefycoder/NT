43c85778bdc30ec7feb0f05ee95f0d5baec53808
tefy <tefysud21@gmail.com>
2017-07-17 22:48:22 -0500

    adding other elements

--- js/scripts.js
+++ js/scripts.js
@@ -97,3 +97,49 @@ $('a[href*=#]:not([href=#])').click(function() {
 		}
 	}
 });
+
+/***************** nosotros ******************/
+$(document).ready(function () {
+    $('#myCarousel').carousel({
+        interval: 10000
+    })
+    $('.fdi-Carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
+
+/*******CONTACTO****/
+
+var $overlay=$('.overlay'),
+    $trigger=$('.circle');
+
+$trigger.click(function(){
+  $overlay.addClass('open');
+  $trigger.addClass('open');
+})
+
+$('.close-button').click(function(){
+  $overlay.removeClass('open');
+  $trigger.removeClass('open');
+})
+
+$('.btn-send').click(function(){
+  if($(this).valid()){
+    document.getElementById("client").reset();
+    $overlay.removeClass('open');
+    $trigger.removeClass('open');/*
+    $('.wrap').addClass('fly');*/
+  }
+})
+
